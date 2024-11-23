8b23dd5e90dd79d1d627ab9adb58b03622dfac2b
Anton Heitz <anton.j.heitz@gmail.com>
2024-11-23 12:41:39 +0100

    Created PyPi structures #1

--- examples/run_example.py
+++ examples/run_example.py
@@ -1,11 +1,11 @@
 import json
 
-from xbrl_generation import generate, File
+from xbrl_forge import create_xbrl
 
 with open("examples/stylesheet.css", "r") as f:
     style_data = f.read()
 with open("examples/ESEF-ixbrl.json", "r") as f:
     data = json.loads(f.read())
 
-results: File = generate(data, styles=style_data)
+results = create_xbrl(data, styles=style_data)
 results.save_file("examples/result", True)
